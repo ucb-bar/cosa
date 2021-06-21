21d1a7b91056dd8e8b26bdd6f297f0df24dc2106
Jenny Huang <hqjenny@hotmail.com>
2021-06-20 20:28:26 -0700

    Add code to update mapping for valid input and run different models

--- src/run_config.py
+++ src/run_config.py
@@ -1,6 +1,7 @@
 import logging
 import pathlib
 import shutil
+from functools import reduce
 
 import utils
 from parse_workload import *
@@ -12,6 +13,48 @@ logger.setLevel(logging.DEBUG)  # capture everything
 def run_config(mapspace, spatial_config, perm_config, factor_config, status_dict=dict(), run_gen_map=True,
                run_gen_tc=False, run_sim_test=False, output_path='output_dir', spatial_configs=None, exe='sim_test',
                valid_check=False, nb_sim=False, outer_loopcount_limit=256, delete_invalid=False):
+    logger.debug("factor_config: {}".format(factor_config))
+    mapspace.update_mapspace(perm_config, factor_config)
+
+    valid, invalid_mem_entries, invalid_mem_instances, utilized_mem_entries, utilized_mem_instances = mapspace.valid_check()
+    while (not valid):
+        mapspace.reset_mapspace(spatial_config, spatial_configs)
+        update_greedy_factor_config(mapspace, invalid_mem_entries, invalid_mem_instances, factor_config)
+        logging.debug("--------------")
+        logging.debug("factor_config: {}".format(factor_config))
+        logging.debug("factor_space_tup: {}".format(mapspace.factor_space_tup))
+        mapspace.update_mapspace(perm_config, factor_config)
+        valid, invalid_mem_entries, invalid_mem_instances, utilized_mem_entries, utilized_mem_instances = mapspace.valid_check()
+
+    if valid:
+        status_dict = run_simulation(mapspace, spatial_config, perm_config, factor_config, status_dict,
+                                     run_gen_map=run_gen_map, run_gen_tc=run_gen_tc, run_sim_test=run_sim_test,
+                                     output_path=output_path, spatial_configs=spatial_configs)
+
+        logging.debug("utilized_mem_entries {}".format(utilized_mem_entries))
+        utilized_mem_entries_prod = reduce(lambda x, y: float(x) * float(y), utilized_mem_entries[0:-1])
+        utilized_mem_entries_sum = reduce(lambda x, y: x + y, utilized_mem_entries[0:-1])
+        logging.debug(
+            "utilized_mem_entries sum={}, prod={:e}".format(utilized_mem_entries_sum, utilized_mem_entries_prod))
+
+        logging.debug("timeloop utilized_capacity {}".format(status_dict['utilized_capacity']))
+        for idx, utilized_capacity in enumerate(utilized_mem_entries):
+            if idx == len(utilized_mem_entries) - 1:
+                continue
+            try:
+                assert (utilized_capacity == status_dict['utilized_capacity'][idx])
+            except:
+                logging.error("utilized_capacity {}".format(utilized_capacity))
+                logging.error("timeloop utilized_capacity {}".format(status_dict['utilized_capacity']))
+                raise RuntimeError('Schedule passed CoSA valid check but failed Timeloop simulation.')
+    else:
+        logging.debug("utilized_mem_entries {}".format(utilized_mem_entries))
+    return status_dict
+
+
+def run_simulation(mapspace, spatial_config, perm_config, factor_config, status_dict=dict(), run_gen_map=True,
+                   run_gen_tc=False, run_sim_test=False, output_path='output_dir', spatial_configs=None, exe='sim_test',
+                   valid_check=False, nb_sim=False, outer_loopcount_limit=256, delete_invalid=False):
     mapspace.reset_mapspace(spatial_config, spatial_configs)
     mapspace.update_mapspace(perm_config, factor_config)
 
@@ -61,7 +104,6 @@ def run_config(mapspace, spatial_config, perm_config, factor_config, status_dict
     output_base = pathlib.Path(output_path).resolve()
     output_dir = output_base / mapspace.arch.config_str() / mapspace.prob.config_str() / mapspace.config_str()[0] / \
                  mapspace.config_str()[1]
-    # print(output_dir)
 
     status_dict[status_dict_key]['output_dir'] = str(output_dir)
 
@@ -79,7 +121,6 @@ def run_config(mapspace, spatial_config, perm_config, factor_config, status_dict
     # logging.debug("status_dict_before: {}".format(status_dict[status_dict_key]))
     # generate map 
     if run_gen_map:
-        # print('run_timeloop> timeloop-model {} {} {}'.format(mapspace.arch.path, mapspace.prob.path, map_path))
         if map_path.exists() and not xml_file.exists():
             status_dict[status_dict_key]['run_status'][0] = 0
         elif not xml_file.exists():
@@ -147,3 +188,58 @@ def get_spatial_size(mapspace, spatial_config, perm_config, factor_config, statu
     for var in ['Weights', 'Inputs', 'Outputs']:
         sizes.append(status_dict['cost']['{}_milp_spatial'.format(var)])
     return sizes
+
+
+def update_greedy_factor_config(mapspace, invalid_mem_entries, invalid_mem_instances, factor_config, move_outter=True):
+    # first resolve invalid_mem_instances
+    for mem_idx in invalid_mem_instances:
+        # min_idx = None
+        # min_mem_idx = mapspace.total_factor_choices - 1
+        for prob_idx, (prob_factor_configs, prob_factors) in enumerate(
+                zip(factor_config, mapspace.unschduled_prob_factors)):
+            if (len(prob_factors) != 1 or prob_factors[0] != 1):
+                # if prob idx is within the mem 
+                for i, factor_mem_idx in enumerate(prob_factor_configs):
+                    if factor_mem_idx >= mapspace.arch.mem_levels:
+                        valid_spatial_mem_idx = factor_mem_idx - mapspace.arch.mem_levels
+                        spatial_mem_idx, _ = mapspace.valid_spatial_levels[valid_spatial_mem_idx]
+
+                        if mem_idx <= spatial_mem_idx:
+                            if factor_config[prob_idx][i] - 1 >= mapspace.arch.mem_levels:
+                                factor_config[prob_idx][i] -= 1
+                            else:
+                                factor_config[prob_idx][i] = 1
+                            return
+
+    for mem_idx in invalid_mem_entries:
+        bound = len(factor_config)
+        prob_idxs = []
+        factor_mem_idxs = []
+        for prob_idx in range(bound - 1, -1, -1):
+            prob_factor_configs = factor_config[prob_idx]
+            prob_factors = mapspace.unschduled_prob_factors[prob_idx]
+            # for prob_idx, (prob_factor_configs, prob_factors) in enumerate(zip(factor_config, mapspace.unschduled_prob_factors)):
+            # if mem_idx in mapspace.prob_mem[prob_idx]:
+            if mapspace.is_mem_related_to_prob(mem_idx, prob_idx):
+                if (len(prob_factors) != 1 or prob_factors[0] != 1):
+                    # if prob idx is within the mem 
+                    for i, factor_mem_idx in enumerate(prob_factor_configs):
+                        # if mapped to temperal 
+                        if factor_mem_idx < mapspace.arch.mem_levels:
+                            if factor_mem_idx <= mem_idx:
+                                if not move_outter:
+                                    factor_config[prob_idx][i] = mem_idx + 1
+                                    return
+                                else:
+                                    prob_idxs.append((prob_idx, i))
+                                    factor_mem_idxs.append(factor_mem_idx)
+        if move_outter:
+            # select one value, must exist 
+            max_mem_idx = factor_mem_idxs[0]
+            select_prob_idx = prob_idxs[0]
+            for i, factor_mem_idx in enumerate(factor_mem_idxs):
+                if factor_mem_idx > max_mem_idx:
+                    max_mem_idx = factor_mem_idx
+                    select_prob_idx = prob_idxs[i]
+            factor_config[select_prob_idx[0]][select_prob_idx[1]] = mem_idx + 1
+            return
---
+++ src/run_dnn_models.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3 
+import argparse
+import logging
+import os
+import pathlib
+
+import utils
+from cosa import run_timeloop
+from cosa_input_objs import Prob
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)  # capture everything
+# logger.disabled = True
+
+_COSA_DIR = os.environ['COSA_DIR']
+
+
+def construct_argparser():
+    parser = argparse.ArgumentParser(description='Run Configuration')
+
+    parser.add_argument('-o',
+                        '--output_dir',
+                        type=str,
+                        help='Output Folder',
+                        default='output_dir',
+                        )
+    parser.add_argument('-ap',
+                        '--arch_path',
+                        type=str,
+                        help='Hardware Architecture Path',
+                        default=f'{_COSA_DIR}/configs/arch/simba.yaml',
+                        )
+    parser.add_argument('-mp',
+                        '--mapspace_path',
+                        type=str,
+                        help='Mapspace Path',
+                        default=f'{_COSA_DIR}/configs/mapspace/mapspace.yaml',
+                        )
+    parser.add_argument('-w',
+                        '--workload_dir',
+                        type=str,
+                        help='DNN Model Workload Folder',
+                        default=f'{_COSA_DIR}/configs/workloads',
+                        )
+    parser.add_argument('-m',
+                        '--model',
+                        type=str,
+                        help='DNN Model',
+                        )
+    return parser
+
+
+def run_dnn_models(workload_dir, arch_path, mapspace_path, output_dir, model):
+    if model is not None:
+        model_strs = [model]
+    else:
+        model_strs = ['alexnet', 'resnet50', 'resnext50_32x4d', 'deepbench']
+    full_results = {'energy': {}, 'pe_cycle': {}, 'cycle': {}, }
+
+    for model_str in model_strs:
+        logger.info(f'model: {model_str}')
+        for k in full_results.keys():
+            full_results[k][model_str] = {}
+
+        model_dir = workload_dir / (model_str + '_graph')
+        layer_def_path = model_dir / 'unique_layers.yaml'
+        layers = utils.parse_yaml(layer_def_path)
+
+        # Schedule each layer
+        for layer_idx, layer in enumerate(layers):
+            prob_path = model_dir / (layer + '.yaml')
+            status_dict = run_timeloop(prob_path, arch_path, mapspace_path, output_dir)
+            status_dict_key = next(iter(status_dict))
+
+            prob = Prob(prob_path)
+            logger.info(f'prob_path: {prob_path}')
+            try:
+                for k in full_results:
+                    full_results[k][model_str][prob.config_str()] = status_dict[status_dict_key][k]
+            except:
+                # Set to -1 and move on the next config
+                full_results[k][model_str][prob.config_str()] = -1
+
+    for k in full_results:
+        full_result_path = pathlib.Path(output_dir) / f'results_{arch_path.stem}_{k}.json'
+        utils.store_json(full_result_path, full_results[k], indent=2)
+
+
+if __name__ == "__main__":
+    parser = construct_argparser()
+    args = parser.parse_args()
+
+    workload_dir = pathlib.Path(args.workload_dir).resolve()
+    arch_path = pathlib.Path(args.arch_path).resolve()
+    mapspace_path = pathlib.Path(args.mapspace_path).resolve()
+    output_dir = args.output_dir
+    model = args.model
+
+    run_dnn_models(workload_dir, arch_path, mapspace_path, output_dir, model)
