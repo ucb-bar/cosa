bcf258755be7d79ab5cf14add25af430d9cde7f8
Qijing Jenny Huang <jennyhuang@nvidia.com>
2022-04-01 00:10:58 -0700

    Add calibration data

---
+++ gemmini/calibration/run.sh
@@ -0,0 +1,17 @@
+for i in {0..3}; do
+    mkdir -p output_dir_${i}
+    mkdir -p output_dir_${i}_imperfect
+    timeloop-model ../arch/* ../prob/mm/mm_${i}.yaml mapspace/mapping${i}.yaml -o output_dir_${i}
+    timeloop-model ../arch/* ../prob/mm/mm_${i}.yaml mapspace/mapping${i}_imperfect.yaml -o output_dir_${i}_imperfect
+done
+
+
+for i in {0..3}; do
+    echo "mm_${i} perfect factorization"
+    grep 'Cycles: ' output_dir_${i}/timeloop-model.stats.txt 
+    grep 'Energy: ' output_dir_${i}/timeloop-model.stats.txt
+    echo "mm_${i} imperfect factorization"
+    grep 'Cycles: ' output_dir_${i}_imperfect/timeloop-model.stats.txt 
+    grep 'Energy: ' output_dir_${i}_imperfect/timeloop-model.stats.txt
+
+done
