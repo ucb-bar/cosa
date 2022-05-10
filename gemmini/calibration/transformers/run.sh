b1577a79e79c200fec995f1c4d61066c241b5c17
jmk <mkang@eecs.berkeley.edu>
2022-05-10 15:46:35 -0700

    TL Transformer Calibration code

--- gemmini/calibration/transformers/run.sh
+++ gemmini/calibration/transformers/run.sh
@@ -9,16 +9,25 @@
 # for i in {0..7}; do
 #     # echo "mm_${i} perfect factorization"
 #     # grep 'Cycles: ' output_dir_${i}/timeloop-model.stats.txt 
-#     # grep 'Energy: ' output_dir_${i}/timeloop-model.stats.txt
-#     echo "mm_${i} imperfect factorization"
-#     grep 'Cycles: ' output_dir_${i}_imperfect/timeloop-model.stats.txt 
-#     grep 'Energy: ' output_dir_${i}_imperfect/timeloop-model.stats.txt
+BERT_ARCH="bert_512x8"
+i=$1
 
-# done
+FUSED_DIR="output_${BERT_ARCH}_${i}"
+NON_FUSED_DIR="output_${BERT_ARCH}_noFused_${i}"
 
-i=0
-mkdir -p output_dir_T512_${i}
-timeloop-model ../../arch/gemmini_transformer.yaml ../../transformer_512/mm_${i}.yaml mapspace/transformer_512_${i}.yaml -o output_dir_T512_${i}
+mkdir -p $FUSED_DIR
+mkdir -p $NON_FUSED_DIR
+timeloop-model ../../arch/gemmini_transformer.yaml ../../prob/transformer_512/mm_${i}.yaml mapspace/${BERT_ARCH}_${i}.yaml -o ${FUSED_DIR}
+timeloop-model ../../arch/arch.yaml ../../prob/transformer_512/mm_${i}.yaml mapspace/${BERT_ARCH}_${i}.yaml -o ${NON_FUSED_DIR}
+
+echo "mm_${i} with BW constraint"
+grep 'Cycles: ' ${FUSED_DIR}/timeloop-model.stats.txt
+grep 'Energy: ' ${FUSED_DIR}/timeloop-model.stats.txt
+echo "mm_${i} without BW constraint"
+grep 'Cycles: ' ${NON_FUSED_DIR}/timeloop-model.stats.txt
+grep 'Energy: ' ${NON_FUSED_DIR}/timeloop-model.stats.txt
+
+
 
 
 
