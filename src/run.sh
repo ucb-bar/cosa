a6c6c2fe1344d8d695150f32eef072f67d7b5d74
Jenny Huang <hqjenny@hotmail.com>
2022-05-10 10:20:24 -0700

    Add mapping comparison script

---
+++ src/run.sh
@@ -0,0 +1,20 @@
+for i in {0..7}; do
+    #mkdir -p output_dir_${i}
+    # mkdir -p output_dir_${i}_imperfect
+    # timeloop-model ../arch/* ../prob/mm/mm_${i}.yaml mapspace/mapping${i}.yaml -o output_dir_${i}
+    # timeloop-model ../arch/* ../prob/mm/mm_${i}.yaml mapspace/mapping${i}_imperfect.yaml -o output_dir_${i}_imperfect
+    # python cosa.py -pp ../gemmini/prob/mm/mm_${i}.yaml  -o output_mm_${i}
+    echo $i
+done
+
+
+# for i in {0..7}; do
+for i in {0..7}; do
+    # echo "mm_${i} perfect factorization"
+    # grep 'Cycles: ' output_dir_${i}/timeloop-model.stats.txt 
+    # grep 'Energy: ' output_dir_${i}/timeloop-model.stats.txt
+    echo "mm_${i} imperfect factorization"
+    grep 'Cycles: ' output_mm_${i}/timeloop-model.stats.txt 
+    grep 'Energy: ' output_mm_${i}/timeloop-model.stats.txt
+
+done
